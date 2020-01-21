0b40e953406d8ca3f1a197d4daef4205947e6cee
Maksym Teslenko <maksteslenko924@gmail.com>
2020-01-21 16:45:35 +0200

    creating structure and routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 
-const App = ()=>{
+/*pages*/
+import HelloPage from "./components/pages/helloPage/HelloPage";
+import SigninPage from "./components/pages/loginPages/SigninPage";
+import SignupPage from "./components/pages/loginPages/SignupPage";
+import NewsPage from "./components/pages/news/NewsPage";
+import EventsPage from "./components/pages/events/EventsPage";
+import UserProfile from "./components/pages/user/UserPage";
 
+/*router */
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+
+const App = ()=>{
+  return(
+    <Router>
+      <div className="App">
+          <Route path="/" exact component={HelloPage} />
+          <Route path="/sign-in" component={SigninPage} />
+          <Route path="/sign-up" component={SignupPage} />
+          <Route path="/news" component={NewsPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/user" component={UserProfile}/>
+      </div>
+    </Router>
+  )
 }
 export default App;
