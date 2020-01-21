49f52603b509bdae21035801bb5ef01e059fb3fe
FireWall-e <holyhostell@gmail.com>
2020-01-22 01:14:23 +0200

    Module rename / Token api init

--- src/api/fakeApi.js
+++ src/api/fakeApi.js
@@ -1,16 +1,86 @@
+import jwt from 'jsonwebtoken';
+
 /**
  * Initialize fakeApi as a global variable
  */
-const fakeApi = (() => {
+const FakeApi = (() => {
 
     /**
-     * Private module variables 
+     * Private module common variables 
      */
-    const privateVariableExample = 'I am visible';
+    /**
+     * Server response emulation
+     * @param {Function} method
+     * @returns {Promise} Returns Promise instance
+     * that will be solved / rejected within response
+     * delay time
+     */
+    const newPromise = (method) => {
+        const delayMS = 1500;
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                method(resolve, reject);
+            }, delayMS);
+        });
+    }
 
     /**
-     * Api's factory methods
+     * Api's entities
      */
+    /**
+     * JWT API wrapper
+     * https://github.com/auth0/node-jsonwebtoken#usage
+     */
+    function Token() {
+        const secretKey = 'evok9nrnbe';
+
+        /**
+         * Store session token within localStorage
+         * @param {String} token 
+         */
+        const set = (token) => {
+            if (token) localStorage.setItem('token', token);
+        }
+
+        /**
+         * Retrieve token from localStorage
+         * @returns {String} Return current session token
+         */
+        this.get = () => {
+            return localStorage.getItem('token');
+        };
+
+        /**
+         * Async session token verification
+         * @returns {Promise}
+         */
+        this.verify = () => {
+            return newPromise((resolve, reject) => { 
+                jwt.verify(this.getToken(), secretKey, function(error, decoded) {
+                    if (error) reject(error);
+                    resolve(decoded);
+                });
+            });
+        };
+        
+        /**
+         * Method to be called on user's
+         * sign in or sign up
+         * @param {Object} payload Data to be tokenized
+         * @param {Object} options JWT options
+         */
+        this.createToken = (payload, options = {}) => {
+            return newPromise((resolve, reject) => { 
+                jwt.sign(payload, secretKey, options, (error, token) => {
+                    if (error) reject(error);
+                    set(token);
+                    resolve(token);
+                });
+            });
+        };
+    }
+
     function Auth() {
 
     }
@@ -21,19 +91,6 @@ const fakeApi = (() => {
 
     function News() {
 
-        /**
-         * Private methods
-         */
-        const privateMethodExample = () => {
-            return 'Johny, and ' + privateVariableExample;
-        }
-
-        /**
-         * Public methods
-         */
-        this.publicMethodExample = () => {
-            console.log('Its me, ', privateMethodExample());
-        };
     }
 
     function Events() {
@@ -44,6 +101,7 @@ const fakeApi = (() => {
      * Return public instances
      */
     return {
+        Token: new Token(),
         Auth: new Auth(),
         User: new User(),
         News: new News(),
@@ -51,4 +109,4 @@ const fakeApi = (() => {
     };
 })();
 
-export default fakeApi;
+export default FakeApi;
