b0359c02f9adfbd7bb056d45ea656e9ceacf8a73
FireWall-e <holyhostell@gmail.com>
2020-01-24 22:28:25 +0200

    Css module added, NewsPage conditional render added

---
+++ src/components/pages/news/NewsList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function NewsList(props) {
+    return (
+        <h1>List</h1>
+    );
+}
--- src/components/pages/news/NewsPage.js
+++ src/components/pages/news/NewsPage.js
@@ -1,20 +1,31 @@
 import React, { Component, Fragment } from "react";
 import NewsItem from "./NewsItem";
-
 import Navbar from "../../layout/navbar/Navbar";
 import Sidebar from "../../layout/sidebar/Sidebar";
+
+import classes from "./NewsPage.module.css";
 
 export default class NewsPage extends Component{
     constructor(props) {
         console.log('props are ', props);
         super(props);
+
+        this.newsIdQueryParameter = new URLSearchParams(this.props.location.search).get('id');
     }
 
     render(){
         return(
             <Fragment>
                 <Navbar/>
-                <Sidebar/>
+                <div className={classes.content}>
+                    <Sidebar/>
+                    <main className={classes.news}>
+                        {this.newsIdQueryParameter
+                         ? <NewsItem />
+                         : console.log('fail')
+                        }
+                    </main>
+                </div>
             </Fragment>
             
         )
