4c0c996745ff5a0cacbd081c7eec3e80fe264bd7
FireWall-e <holyhostell@gmail.com>
2020-01-24 14:14:01 +0200

    Component structure improved

--- src/components/pages/news/NewsPage.js
+++ src/components/pages/news/NewsPage.js
@@ -1,6 +1,12 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import NewsItem from './NewsItem';
 
-export default class NewsPage extends Component{
+export default class NewsPage extends Component {
+    constructor(props) {
+        console.log('props are ', props);
+        super(props);
+    }
+
     render(){
         return(
             <h1>News!!!</h1>
