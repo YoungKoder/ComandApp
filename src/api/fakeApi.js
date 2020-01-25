977ae5f17b87124f8d7811814ac44d84ad0f0e09
FireWall-e <holyhostell@gmail.com>
2020-01-26 01:15:52 +0200

    FakeApi news submodule & news component improvements

--- src/api/fakeApi.js
+++ src/api/fakeApi.js
@@ -162,12 +162,12 @@ const FakeApi = (() => {
                     const newsItemId = Date.now();
                     const newsItem = { [`${newsItemId}`]: newsItemData };
                     const news = Object.assign(
-                        newsItem,
-                        JSON.parse(localStorage.getItem('news') || '{}')
+                        JSON.parse(localStorage.getItem('news') || '{}'),
+                        newsItem
                     );
 
                     localStorage.setItem('news', JSON.stringify(news));
-                    resolve(newsItem, news);
+                    resolve([news, newsItem]);
                 })
                 .catch(error => reject(error));
             });
@@ -210,9 +210,13 @@ const FakeApi = (() => {
                 Token.verify()
                 .then(decoded => {
                     const newsJsonString = localStorage.getItem('news');
-                    if (!newsJsonString) reject(new Error('There is no news!'));
                     const news = JSON.parse(newsJsonString);
-                    resolve(newsItemId ? news[newsItemId] : news);
+                    if (newsItemId) {
+                        if (!news) reject(new Error('There is no news!'));
+                        if (!news[newsItemId]) reject(new Error('There is no news with such id!'));
+                        resolve(news[newsItemId]);
+                    }
+                    resolve(news);
                 })
                 .catch(error => reject(error));
             });
--- src/components/pages/news/NewsListPage.js
+++ src/components/pages/news/NewsListPage.js
@@ -6,22 +6,55 @@ import NewsItem from "./NewsItem";
 import classes from "./NewsListPage.module.css";
 
 export default class NewsListPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            newsList: {}
+        };
+    }
+
+    addNewsItem = () => {
+        News.add()
+        .then(newsData => {
+            console.log('newsList is ', newsData[0]);
+            this.setState({newsList: newsData[0]}, () => console.log('this state is ', this.state.newsList));
+        })
+        .catch(error => console.log(error))
+    }
+
+    deleteNews = (event, newsItemId = '') => {
+        console.log('newsItemId', newsItemId);
+        News.delete(newsItemId)
+        .then(newsList => this.setState({ newsList }))
+        .catch(error => console.log(error));
+    }
+
+    getNewsList = () => {
+        News.get()
+        .then(newsList => newsList && this.setState({ newsList }))
+        .catch(error => console.log(error));
+    }
+
+    componentDidMount() {
+        this.getNewsList();
+    }
+
     render() {
-        console.log('newslistpage props are ', this.props);
-        // const NewsItem = this.props.NewsItem;
-        // const addNewsItem = this.props.addNewsItemFunction;
+        let newsItems = [];
+        Object.keys(this.state.newsList).map(newsItemid => {
+            newsItems.push(<NewsItem key={newsItemid} data={this.state.newsList[newsItemid]} />);
+        });
+        console.log('news items are ', newsItems);
         return (
             <>
-                {/* {<props.NewsItem />} */}
                 <div className={classes['news__controls']}>
-                    <Button onClick={
-                        () => News.add()
-                              .then(newsItem => console.log('new news id ', newsItem))
-                              .catch(error => console.log(error))
-                    }>Add</Button>
+                    <Button onClick={this.addNewsItem}>Add</Button>
+                    <Button onClick={this.deleteNews}>Delete All</Button>
                 </div>
-                {/* <div className={classes['news__list']}>{<NewsItem />}{<NewsItem />}</div> */}
-                <div className={classes['news__list']}>{<NewsItem />}{<NewsItem />}</div>
+                <div className={classes['news__list']}>
+                    {newsItems}
+                </div>
             </>
         );
     }
